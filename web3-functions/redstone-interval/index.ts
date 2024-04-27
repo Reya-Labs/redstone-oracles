597a90b4499450d14606cbf5800e18a0c754c8cd
Artur Begyan <arturbegyan98@gmail.com>
2024-04-27 23:37:20 +0100

    feat: process multiple feeds in one go

--- web3-functions/redstone-interval/index.ts
+++ web3-functions/redstone-interval/index.ts
@@ -1,6 +1,6 @@
 import {
   Web3Function,
-  Web3FunctionContext,
+  Web3FunctionContext
 } from "@gelatonetwork/web3-functions-sdk";
 import { BigNumber, Contract, ethers } from "ethers";
 
@@ -12,7 +12,7 @@ const parsePrice = (value: Uint8Array) => {
 };
 
 Web3Function.onRun(async (context: Web3FunctionContext) => {
-  const { userArgs, multiChainProvider, secrets, storage } = context;
+  const { userArgs, multiChainProvider } = context;
 
   const provider = multiChainProvider.default();
 
@@ -21,52 +21,59 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
     "function getDataServiceId() public pure  override returns (string memory)",
     "function getUniqueSignersThreshold() public pure returns (uint8)",
     "function latestRoundData() external view returns (uint80,int256,uint256,int256,uint80)",
-    "function decimals() external view returns (uint8)",
+    "function decimals() external view returns (uint8)"
   ];
 
-  const priceFeedAdapterAddress = userArgs.priceFeedAdapterAddress as string;
+  // todo: input validation lengths should be the same
 
-
-  const priceFeed = userArgs.priceFeed as string;
-  if (priceFeed == undefined){
-    return { canExec:false, message:"No price feed arg"}
-  }
-
-
-  const priceFeedAdapter = new Contract(priceFeedAdapterAddress, abi, provider);
-
+  const priceFeedAdapterAddresses: string[] = userArgs.priceFeedAdapterAddresses as string[];
+  const priceFeeds: string[] = userArgs.priceFeeds as string[];
 
   const getLatestSignedPrice = await sdk.requestDataPackages({
     dataServiceId: "redstone-primary-prod",
     uniqueSignersCount: 3,
-    dataFeeds: [priceFeed],
-    urls: ["https://oracle-gateway-1.a.redstone.finance"],
+    dataFeeds: priceFeeds,
+    urls: ["https://oracle-gateway-1.a.redstone.finance"]
   });
 
-  // Wrap contract with redstone data service
-  const wrappedOracle =
-    WrapperBuilder.wrap(priceFeedAdapter).usingDataService(
-      getLatestSignedPrice
+  const callDataResults: { to: string; data: string }[] = [];
+
+  for (let i = 0; i < priceFeedAdapterAddresses.length; i++) {
+    if (priceFeeds[i] == undefined) {
+      return { canExec: false, message: "No price feed arg" };
+    }
+    const priceFeedAdapter = new Contract(
+      priceFeedAdapterAddresses[i],
+      abi,
+      provider
     );
+    // Wrap contract with redstone data service
+    const wrappedOracle = WrapperBuilder.wrap(
+      priceFeedAdapter
+    ).usingDataService(getLatestSignedPrice);
 
-  // Retrieve stored & live prices
+    // Retrieve stored & live prices
 
-  const { dataPackage } = getLatestSignedPrice[priceFeed]![0];
+    const { dataPackage } = getLatestSignedPrice[priceFeeds[i]]![0];
 
-  const parsedPrice = parsePrice(dataPackage.dataPoints[0].value);
-
-  // Craft transaction to update the price on-chain
-  console.log(`Setting ${priceFeed} price in PriceFeed contract to: ${parsedPrice}`);
-  const { data } =
-    await wrappedOracle.populateTransaction.updateDataFeedsValues(
+    const parsedPrice = parsePrice(dataPackage.dataPoints[0].value);
+    // Craft transaction to update the price on-chain
+    console.log(
+      `Setting ${priceFeeds[i]} price in PriceFeed contract to: ${parsedPrice}`
+    );
+    const {
+      data
+    } = await wrappedOracle.populateTransaction.updateDataFeedsValues(
       dataPackage.timestampMilliseconds
     );
- 
-
-    return {
-      canExec: true,
-      callData: [{ to: priceFeedAdapterAddress, data: data as string }],
+    callDataResults[i] = {
+      to: priceFeedAdapterAddresses[i],
+      data: data
     };
+  }
 
-
+  return {
+    canExec: true,
+    callData: callDataResults
+  };
 });
